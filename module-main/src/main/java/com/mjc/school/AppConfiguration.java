cc5b1dfaaca78d691ca0b59dca48a4b21720747e
KZadka <kzadka89@gmail.com>
2023-01-24 12:41:32 +0100

    Updated configuration class to get aspects working

--- module-main/src/main/java/com/mjc/school/AppConfiguration.java
+++ module-main/src/main/java/com/mjc/school/AppConfiguration.java
@@ -4,9 +4,11 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
 @ComponentScan(basePackages = {"com.mjc.school", "com.mjc.school.*"})
+@EnableAspectJAutoProxy
 public class AppConfiguration {
 
     @Bean
